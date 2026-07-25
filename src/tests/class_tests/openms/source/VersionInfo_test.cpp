b2e0f7048b028a32a18616d0664d810f594107ad
pjones <pjones@users.noreply.github.com>
2026-07-25 15:05:45 +0000

    Updated OpenMS package version number to 0.1.0

--- src/tests/class_tests/openms/source/VersionInfo_test.cpp
+++ src/tests/class_tests/openms/source/VersionInfo_test.cpp
@@ -41,8 +41,8 @@ END_SECTION
 START_SECTION((static VersionDetails getVersionStruct()))
 {
   VersionInfo::VersionDetails detail;
-  detail.version_major = 3;
-  detail.version_minor = 6;
+  detail.version_major = 0;
+  detail.version_minor = 1;
   detail.version_patch = 0;
   TEST_EQUAL(VersionInfo::getVersionStruct().version_major, detail.version_major);
   TEST_EQUAL(VersionInfo::getVersionStruct().version_minor, detail.version_minor);
